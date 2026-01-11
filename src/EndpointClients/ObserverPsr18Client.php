c5a72ea72e6aff6c4da7f54dd1b06de0b67fc72f
rkr <ron.kirschler@gmail.com>
2026-01-11 18:01:50 +0100

    Add PSR-18 HTTP client support and ObserverPsr18Client implementation

---
+++ src/EndpointClients/ObserverPsr18Client.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Observer\EndpointClients;
+
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
+class ObserverPsr18Client implements ObserverEndpointClient {
+	/**
+	 * @param string $authToken
+	 * @param ClientInterface $httpClient
+	 * @param RequestFactoryInterface $requestFactory
+	 * @param StreamFactoryInterface $streamFactory
+	 * @param string $userAgent
+	 */
+	public function __construct(
+		private string $authToken,
+		private ClientInterface $httpClient,
+		private RequestFactoryInterface $requestFactory,
+		private StreamFactoryInterface $streamFactory,
+		private string $userAgent = 'ObserverClient v2'
+	) {}
+
+	/**
+	 * @param array $data
+	 * @return string
+	 */
+	private function encodeForm(array $data) {
+		if (defined('PHP_QUERY_RFC3986')) {
+			return http_build_query($data, '', '&', PHP_QUERY_RFC3986);
+		}
+		return http_build_query($data, '', '&');
+	}
+
+	/**
+	 * @param string $url
+	 * @param array $data
+	 * @return array
+	 */
+	public function call($url, array $data) {
+		$request = $this->requestFactory->createRequest('POST', $url)
+			->withHeader('X-Token', $this->authToken)
+			->withHeader('User-Agent', $this->userAgent)
+			->withHeader('Content-Type', 'application/x-www-form-urlencoded')
+			->withHeader('Accept', 'application/json');
+
+		$request = $request->withBody($this->streamFactory->createStream($this->encodeForm($data)));
+
+		try {
+			$response = $this->httpClient->sendRequest($request);
+		} catch (\Exception $e) {
+			return [];
+		}
+
+		$decoded = @json_decode((string) $response->getBody(), true);
+		if (!is_array($decoded)) {
+			return [];
+		}
+
+		return $decoded;
+	}
+}
